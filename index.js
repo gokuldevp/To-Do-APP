396a9e9f005391dbea5162f155340027aa3dd06c
Gokul Dev P <gokuldev.p123@gmail.com>
2023-07-30 11:52:43 +0530

    Second commit

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express()
 const port = 8000;
 
+// middleware to use router
+app.use("/", require('./routes'))
 
-app.listen(port, (err)=>{
+
+app.listen(port, (err) =>{
     if(err){
         console.log(`Error while running the server: ${err}`)
     }
